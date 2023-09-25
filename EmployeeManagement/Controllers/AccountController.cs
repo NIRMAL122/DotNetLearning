a2c0aa6175571e7fb697612bfcb3c31700f52723
Nirmal122 <singhnirmal588@gmail.com>
2023-09-25 11:59:33 +0530

    Add password for external Users

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -391,8 +391,14 @@ namespace EmployeeManagement.Controllers
 
 
         [HttpGet]
-        public IActionResult ChangePassword()
+        public async Task<IActionResult> ChangePassword()
         {
+            var user = await userManager.GetUserAsync(User);
+            var userHasPassword = await userManager.HasPasswordAsync(user);
+            if (!userHasPassword)
+            {
+                return RedirectToAction("AddPassword");
+            }
             return View();
         }
 
@@ -422,6 +428,43 @@ namespace EmployeeManagement.Controllers
                 return View("ChangePasswordConfirmation");
             }
             return View(model);
+        }
+
+        [HttpGet]
+        public  async Task<IActionResult> AddPassword()
+        {
+            var user = await userManager.GetUserAsync(User);
+            var userHasPassword= await userManager.HasPasswordAsync(user);
+            if(userHasPassword)
+            {
+                return RedirectToAction("ChangePassword");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await userManager.GetUserAsync(User);
+                var result = await userManager.AddPasswordAsync(user,
+                    model.NewPassword);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+
+                await signInMangaer.RefreshSignInAsync(user);
+                return View("AddPasswordConfirmation");
+            }
+            return View(model);
+            
         }
 
     }
---
+++ EmployeeManagement/ViewModels/AddPasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class AddPasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword", ErrorMessage =
+            "The new Password and Confirmation password do not Match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
