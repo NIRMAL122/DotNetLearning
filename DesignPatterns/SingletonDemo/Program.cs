ec98de9268382df444b9750dc7f40c09229b2a38
Nirmal122 <singhnirmal588@gmail.com>
2023-09-26 12:05:21 +0530

    Design Pattern

---
+++ DesignPatterns/SingletonDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SingletonDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton obj= Singleton.getInstance;
+            obj.Print("Charlie");
+
+            Singleton obj1 = Singleton.getInstance;
+            obj1.Print("Laurel");
+            
+            Singleton obj2 = Singleton.getInstance;
+            obj1.Print("BonBon");
+        }
+    }
+}
---
+++ DesignPatterns/SingletonDemo/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton getInstance
+        {
+            get
+            {
+                if(instance==null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("object {0}", counter);
+        }
+
+
+
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
